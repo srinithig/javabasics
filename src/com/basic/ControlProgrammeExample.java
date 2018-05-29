6f5b22a12b886af6820587185daede84c75f2b69
srinithig <gsrinithi1997@gmail.com>
2018-05-29 18:06:32 +0530

    basics

---
+++ src/com/basic/ControlProgrammeExample.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class ControlProgrammeExample {
+public static void main(String[]args)
+{
+	int age=65;
+	if(age>=65)
+	System.out.println("Age is greater than or equal to 65");
+	else
+		System.out.println("Age is less than 65");
+	}
+}
---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String []args) {
+String firstName= "Srinithi";
+String lastName="priya";
+System.out.println(firstName+" "+ lastName);
+}
+}
