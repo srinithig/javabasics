cdf1a948f7130f2cf17ab265ad09eda2f00c500d
srinithig <gsrinithi1997@gmail.com>
2018-05-30 15:48:04 +0530

    basic

---
+++ src/com/basic/SwitchPogramme1.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class SwitchPogramme1 {
+	public static void main(String[]args) {
+	int n = 2;
+	switch (n)
+	{
+	case 1:
+	System.out.println( "The number is 1" );
+	case 2:
+	System.out.println( "The number is 2" );
+	break;
+	default:
+	System.out.println( "The number is not 1 or 2" );
+	break;
+	}
+	}
+}
