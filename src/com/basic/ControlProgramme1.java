121b72260e9a00493f0bef7a3c3bcd36dc727e46
srinithig <gsrinithi1997@gmail.com>
2018-05-30 14:37:01 +0530

    basic

---
+++ src/com/basic/ControlProgramme1.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class ControlProgramme1 {
+	public static void main(String[]args)
+	{
+	int	i=0;
+		do {
+			System.out.println("DO while ="   +i);
+			i++;
+		}
+		while (i<10);
+		
+			
+			
+		}
+	}
+
+
