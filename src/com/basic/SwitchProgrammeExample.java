32778d7fd203253684124ee8ce67093cf73ac1a3
srinithig <gsrinithi1997@gmail.com>
2018-05-30 10:05:01 +0530

    basics

---
+++ src/com/basic/SwitchProgrammeExample.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class SwitchProgrammeExample {
+public static void main(String[]args)
+{
+	int choice=30;
+	switch(choice)
+	{
+	case 10:
+	System.out.println(10);
+	break;
+	case 20:
+	System.out.println(20);
+	break;
+	case 30:
+	System.out.println(30);
+	break;
+	default:
+	System.out.println("default");
+	break;
+	}
+	}
+	}
