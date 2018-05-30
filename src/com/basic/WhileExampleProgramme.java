0ad93234ad1dd6ce87a5effbc74d8f02b3b5c633
srinithig <gsrinithi1997@gmail.com>
2018-05-30 10:28:38 +0530

    basics

---
+++ src/com/basic/WhileExampleProgramme.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class WhileExampleProgramme {
+	public static void main(String []args)
+	{
+		int y=0;
+		while (y<0)
+		System.out.println("y= " + y);
+		y++;
+		System.out.println(y);
+		
+	}
+
+}
