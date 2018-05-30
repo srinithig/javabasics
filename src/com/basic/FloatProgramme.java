77df153cd26c513005bb6ca8c6afb8cd14349429
srinithig <gsrinithi1997@gmail.com>
2018-05-30 15:44:46 +0530

    basic

---
+++ src/com/basic/FloatProgramme.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class FloatProgramme {
+	public static void main(String[]args) {
+	float k;
+	
+	{
+	for (k= 1; k != 10; k += 1)
+		System.out.println( k );
+	}
+	}
+}
+
+
+
+
+
