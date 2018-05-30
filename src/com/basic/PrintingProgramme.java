6b3282ebc63f6fbdf0d143373d50903ea70371a1
srinithig <gsrinithi1997@gmail.com>
2018-05-30 15:28:03 +0530

    basic

---
+++ src/com/basic/PrintingProgramme.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class PrintingProgramme {
+	public static void main(String[] args )
+	{
+		int i,j;
+	for (i = 1; i <= 9; i++ )
+	{
+	for (j = 1; j <= 4; j++ )
+	System.out.print( '@' );
+
+	 System.out.println();
+	}
+	}
+}
+
+
+
