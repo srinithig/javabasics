3ad1db38048689c6faa8f22b605a41a68c68d45c
srinithig <gsrinithi1997@gmail.com>
2018-05-30 09:06:52 +0530

    sample

---
+++ src/com/basic/LoopingProgrammeExample.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class LoopingProgrammeExample {
+	public static void main(String []args)
+	{
+		int x=1,total=0;
+		while(x<=10)
+		{
+			total+= x;
+			++x;
+			System.out.println(x);
+		}
+		}
+}
+
