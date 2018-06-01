f927e9a4b1f2f6739d559ace75ec6eed0fc75f75
srinithig <gsrinithi1997@gmail.com>
2018-06-01 15:50:54 +0530

    basic

---
+++ src/com/basic/array.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class array {
+	public static void main(String[] args) {
+		int array[] = new int[10];
+for(int count=0;count<10;count++) {
+	array[count]=count+1;
+	
+	System.out.println("array["+count+"] = "+array[count]);
+	}
+	
+	
+}
+}
+
