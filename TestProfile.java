c5368928392b6a0584d8a05f412608497e99d94b
srinithig <39693577+srinithig@users.noreply.github.com>
2018-06-04 18:07:02 +0530

    Add files via upload

---
+++ TestProfile.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class TestProfile {
+public static void printProfile(IProfile profile){
+	profile.myBasicInfo();
+	profile.myHobbies();
+}
+public static void main(String[] args) {
+	IProfile myProfile=new SoundharyaProfile();
+	TestProfile.printProfile(myProfile);
+}
+	
+}
+
+
