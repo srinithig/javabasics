5af2db2c6f48c878da97b2fa94d4d86b7d4d8f00
srinithig <gsrinithi1997@gmail.com>
2018-06-05 11:31:35 +0530

    basic

---
+++ src/com/basic/SriiiProfile.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class SriiiProfile implements IProfile{
+	@Override
+	public void myBasicInfo() {
+		// TODO Auto-generated method stub
+		System.out.println("FirstName : Sriii");
+		System.out.println("LastName : Nithi");
+	}
+
+	@Override
+	public void myHobbies() {
+		// TODO Auto-generated method stub
+		System.out.println("My hobbies is watching news and dance");
+		
+	}
+
+}
